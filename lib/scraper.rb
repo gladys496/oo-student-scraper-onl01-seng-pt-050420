af4d5174db8e572f8bbe5aae7c5af05a3e17825e
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-29 22:17:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,9 @@ require 'pry'
 class Scraper
   
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML("open(https://learn-co-curriculum.github.io/student-scraper-test-page/"))
+    doc = Nokogiri::HTML(open(index_url))
 
+     scraped_students = []
     
  
   end
