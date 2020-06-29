140f1d611dd6c165313c9eaebf00ffce1e587da1
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-29 22:34:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
 
      scraped_students = []
      
-     oc.css("div.student-card").each do |student|
+     doc.css("div.student-card").each do |student|
       scraped_students << {
         name: student.css("h4.student-name").text, 
         location: student.css("p.student-location").text, profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
