1c2cf5c0604351329464737daed7f0a8f6c01de2
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-29 22:08:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,8 +4,10 @@ class Student
 
   @@all = []
 
-  def initialize(student_hash, name)
-    @name = name 
+  def initialize(student_hash)
+   student_hash.each do |key, value|
+      self.send(("#{key}="), value)
+    end 
    @@all << self 
     
   end
