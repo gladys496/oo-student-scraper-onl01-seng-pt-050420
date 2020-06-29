1450d0f6bd706ea500cd372b261c523bfd68d685
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-29 22:56:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,13 @@ require 'pry'
 class Scraper
   
   def self.scrape_index_page(index_url)
-   doc = Nokogiri::HTML(open(index_url))
-
-     scraped_students = []
-     
-    doc.css("div.student-card").each do |student|
+   doc.css("div.student-card").each do |student|
       scraped_students << {
         name: student.css("h4.student-name").text, 
         location: student.css("p.student-location").text, profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"}
-     
-     
-end 
-  end
+    end
+    scraped_students
+  end	
 
 
   def self.scrape_profile_page(profile_url)
